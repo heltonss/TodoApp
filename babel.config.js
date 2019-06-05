ed86c99a0ddb8888df97f95a30d27e8ad5971121
heltonss <helton.silveira@grupoltm.com.br>
2019-06-05 19:26:12 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable func-names */
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['babel-plugin-root-import', {
+        rootPathSuffix: 'src',
+      }],
+    ],
+    env: {
+      production: {
+        plugins: [
+          ['babel-plugin-root-import', {
+            rootPathSuffix: 'src',
+          }],
+        ],
+      },
+    },
+  };
+};
