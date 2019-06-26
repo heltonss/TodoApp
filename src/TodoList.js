cae669718701e2ba2d4cd5152785c19d5d4fe385
heltonss <helton.silveira@grupoltm.com.br>
2019-06-26 09:12:48 -0300

    add - actions

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,8 @@
+import { bindActionCreators } from 'redux';
 import React, { Component } from 'react';
 import { Button, Text, View } from 'react-native';
 import { connect } from 'react-redux';
+import * as TodoActions from './store/actions/todo';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoList extends Component {
@@ -9,13 +11,13 @@ class TodoList extends Component {
   // }
 
   render() {
-    const { todos, dispatch } = this.props;
+    const { todos, dispatch, markAsCompleted, addTodo } = this.props;
     return (
       <View style={{ flex: 1, backgroundColor: '#fff', justifyContent: 'center' }}>
         {
-          todos.map(todo => <Text onPress={() => dispatch({ type: 'MARK_AS_COMPLETED', id: todo.id })} style={{ textDecorationLine: todo.completed ? 'line-through' : 'none' }} key={todo.id}>{todo.text}</Text>)
+          todos.map(todo => <Text onPress={() => markAsCompleted(todo.id)} style={{ textDecorationLine: todo.completed ? 'line-through' : 'none' }} key={todo.id}>{todo.text}</Text>)
         }
-        <Button title="adicionar" onPress={() => dispatch({ type: 'ADD_TODO', text: 'Novo todo' })} />
+        <Button title="adicionar" onPress={addTodo} />
       </View>
     );
   }
@@ -25,4 +27,7 @@ const mapStateToProps = state => ({
   todos: state,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => (bindActionCreators(TodoActions, dispatch));
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
